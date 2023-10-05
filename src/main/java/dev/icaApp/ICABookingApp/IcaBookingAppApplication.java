2724d79d66f056b6a78eb41eac4428659d3e3760
INNOCENT CHARLES UDO <innocentcharlesudo@gmail.com>
2023-10-05 13:04:45 +0100

    first commit

---
+++ src/main/java/dev/icaApp/ICABookingApp/IcaBookingAppApplication.java
@@ -0,0 +1,13 @@
+package dev.icaApp.ICABookingApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IcaBookingAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IcaBookingAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/dev/icaApp/ICABookingApp/IcaBookingAppApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.icaApp.ICABookingApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IcaBookingAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
